16f7361808a29b113d823ed7792bb20949570922
Mike Wyatt <wyatt.mike@gmail.com>
2019-05-30 03:13:38 -0300

    add appStyles / AppStyles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,8 @@
+import withStyles, {
+  StyleRulesCallback,
+} from '@material-ui/core/styles/withStyles'
+import { PropInjector } from '@material-ui/types'
+
 export { Theme } from '@material-ui/core/styles/createMuiTheme'
 export { default as createStyles } from '@material-ui/core/styles/createStyles'
 export {
@@ -5,3 +10,45 @@ export {
   CSSProperties,
   WithStyles,
 } from '@material-ui/core/styles/withStyles'
+
+// Easy overloads for using material-uis style system.
+// before:
+//
+//     const styles = (theme: Theme) =>
+//       createStyles({
+//         appBar: {
+//           ...,
+//         },
+//       })
+//
+//     function MyComponent(props: WithStyles<typeof styles>) {
+//       return ...
+//     }
+//
+//     export default withStyles(styles)(MyComponent)
+//
+// after:
+//
+//     const styles = appStyles((theme: Theme) => ({
+//       appBar: {
+//         ...,
+//       },
+//     }))
+//
+//     function MyComponent(props: AppStyles<typeof styles>) {
+//       return ...
+//     }
+//
+//     export default styles(MyComponent)
+
+export function appStyles<C extends string>(style: StyleRulesCallback<C>) {
+  return withStyles(style)
+}
+
+// export type AppStyles<T> = T extends PropInjector<WithStyles<infer K>, any>
+//   ? K extends string
+//     ? { classes: ClassNameMap<K> }
+//     : never
+//   : never
+
+export type AppStyles<T> = T extends PropInjector<infer K> ? K : never
