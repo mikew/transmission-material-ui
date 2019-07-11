311221d574b4292be7f3eafed5761abd569819b1
Mike Wyatt <wyatt.mike@gmail.com>
2019-07-11 01:29:03 -0300

    use proper api url when building

--- src/api/apiInstance.ts
+++ src/api/apiInstance.ts
@@ -2,7 +2,9 @@
 import Transmission from '.'
 
 const apiInstance = new Transmission({
-  url: 'http://10.0.1.16:8080/transmission/rpc',
+  url:
+    process.env.REACT_APP_TRANSMISSION_API_URL ||
+    'http://10.0.1.16:8080/transmission/rpc',
 })
 
 export default apiInstance
