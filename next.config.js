b2d85d31610fd510e9cc1993b8c8453a154db8b3
Mike Wyatt <wyatt.mike@gmail.com>
2023-04-08 03:56:07 -0300

    convert to next: pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+const nextPwa = require('@ducanh2912/next-pwa').default
+
 process.env.NEXT_PUBLIC_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || ''
 process.env.NEXT_PUBLIC_TRANSMISSION_RPC_PATH =
   process.env.NEXT_PUBLIC_TRANSMISSION_RPC_PATH ||
@@ -25,3 +27,42 @@ const nextConfig = {
   },
 }
 
+function randomId() {
+  return Math.random().toString(16).slice(2)
+}
+
+const buildId = randomId()
+
+const withPwa = nextPwa({
+  disable: process.env.NODE_ENV === 'development',
+
+  workboxOptions: {
+    additionalManifestEntries: [
+      // Since this is a spa, we can cache the index.html.
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH}/index.html`,
+        revision: buildId,
+      },
+      // No clue why next-pwa doesn't cache the manifest by default.
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH}/manifest.webmanifest`,
+        revision: buildId,
+      },
+    ],
+  },
+
+  // Not sure why you'd do this instead of workbox's manifest that we're doing
+  // above. This is a client-side JS cache, on top of workbox. However, the
+  // documentation states:
+  //     recommend: set to false if your start url always returns same HTML
+  //     document.
+  dynamicStartUrl: false,
+
+  // This needs to be different from wherever `next export` puts our files, that
+  // directory seems to get clobbered and whatever next-pwa puts there is
+  // removed.
+  // The build script copies all files from here to where they need to be.
+  dest: 'pwa-out',
+})
+
+module.exports = withPwa(nextConfig)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,16 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
   return (
     <Html lang="en">
       <Head>
+        <PwaMeta
+          appAuthor=""
+          appDescription=""
+          appLink=""
+          appTitle="Transmission"
+          // This is theme.palette.background.paper mixed with an overlay that
+          // mui adds.
+          themeColor="#302a31"
+        />
+
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
@@ -29,6 +39,92 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
         <NextScript />
       </body>
     </Html>
+  )
+}
+
+const PwaMeta: React.FC<{
+  appTitle: string
+  appDescription: string
+  appAuthor: string
+  appLink: string
+  themeColor: string
+}> = (props) => {
+  return (
+    <>
+      <meta name="application-name" content={props.appTitle} />
+      <meta name="description" content={props.appDescription} />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="theme-color" content={props.themeColor} />
+
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content={props.appTitle} />
+
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content={props.themeColor} />
+      <meta name="msapplication-tap-highlight" content="no" />
+
+      {/* <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" /> */}
+      {/* <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/icons/touch-icon-ipad.png"
+      /> */}
+      {/* <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/touch-icon-iphone-retina.png"
+      /> */}
+      {/* <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="/icons/touch-icon-ipad-retina.png"
+      /> */}
+
+      {/* <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      /> */}
+      {/* <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      /> */}
+      <link
+        rel="manifest"
+        href={`${process.env.NEXT_PUBLIC_BASE_PATH}/manifest.webmanifest`}
+      />
+      {/* <link
+        rel="mask-icon"
+        href="/icons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      /> */}
+      {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content={props.appLink} />
+      <meta name="twitter:title" content={props.appTitle} />
+      <meta name="twitter:description" content={props.appDescription} />
+      {/* <meta
+        name="twitter:image"
+        content="https://yourdomain.com/icons/android-chrome-192x192.png"
+      /> */}
+      <meta name="twitter:creator" content={props.appAuthor} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={props.appTitle} />
+      <meta property="og:description" content={props.appDescription} />
+      <meta property="og:site_name" content={props.appTitle} />
+      <meta property="og:url" content={props.appLink} />
+      {/* <meta
+        property="og:image"
+        content="https://yourdomain.com/icons/apple-touch-icon.png"
+      /> */}
+    </>
   )
 }
 
