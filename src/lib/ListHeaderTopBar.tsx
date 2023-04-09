f0990a060f4c8ca0936daa07dcfbd631cd7d301b
Mike Wyatt <wyatt.mike@gmail.com>
2023-04-09 15:28:56 -0300

    tweak ListHeaderTopBar style

--- src/lib/ListHeaderTopBar.tsx
+++ src/lib/ListHeaderTopBar.tsx
@@ -2,12 +2,15 @@ import ListSubheader from '@mui/material/ListSubheader'
 import { styled } from '@mui/material/styles'
 import { memo } from 'react'
 
+const DENSE_TOOLBAR_HEIGHT = 48
+
 const ListHeaderTopBar = styled(ListSubheader)(({ theme }) => ({
-  top: 48,
+  // -1 because there's some instances where there's a gap.
+  top: DENSE_TOOLBAR_HEIGHT - 1,
   wordBreak: 'break-all',
   // ListSubheader's lineHeight is 48px, since we force ListHeaderTopBar to
   // wrap, that ends up looking ridiculous.
-  lineHeight: theme.typography.overline.lineHeight,
+  lineHeight: theme.typography.caption.lineHeight,
   // backgroundColor: theme.palette.background.paper,
 }))
 
