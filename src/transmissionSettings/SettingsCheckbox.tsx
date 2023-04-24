dd6b3a0a5590faec07faa58ff3e7eaef715f78d8
Mike Wyatt <wyatt.mike@gmail.com>
2023-04-24 17:32:20 -0300

    handle helperText in SettingsCheckbox

--- src/transmissionSettings/SettingsCheckbox.tsx
+++ src/transmissionSettings/SettingsCheckbox.tsx
@@ -1,16 +1,28 @@
 import { ListItem, ListItemIcon, ListItemText } from '@mui/material'
 import { Field } from 'formik'
 import { Checkbox } from 'formik-mui'
+import React from 'react'
 
 export type SettingsCheckboxProps = {
   name: string
   label: React.ReactNode
+  helperText?: React.ReactNode
 }
 
 const SettingsCheckbox: React.FC<SettingsCheckboxProps> = ({
   name,
+  helperText,
   ...props
 }) => {
+  const fieldName = `[${name}]`
+  const finalHelperText = helperText ? (
+    <>
+      {helperText} {fieldName}
+    </>
+  ) : (
+    fieldName
+  )
+
   return (
     <ListItem divider>
       <ListItemIcon>
@@ -24,7 +36,7 @@ const SettingsCheckbox: React.FC<SettingsCheckboxProps> = ({
           // {...props}
         />
       </ListItemIcon>
-      <ListItemText primary={props.label} secondary={`[${name}]`} />
+      <ListItemText primary={props.label} secondary={finalHelperText} />
     </ListItem>
   )
 }
